5a5100a6843bcbe610198e3564e94d49cef1588b
rovinsharmagit <internetsharma45@gmail.com>
2023-10-24 22:23:57 +0530

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
-  return <h1 className="text-red-800">Hello World</h1> 
+  return <Button>Hello World</Button> 
 }
