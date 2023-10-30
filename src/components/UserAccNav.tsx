df513a574b08352723964d96006e065700345b5d
rovinsharmagit <internetsharma45@gmail.com>
2023-10-30 23:41:46 +0530

    Working With User Dropdown In User Navigation

--- src/components/UserAccNav.tsx
+++ src/components/UserAccNav.tsx
@@ -1,6 +1,12 @@
+'use client';
+
 import { User } from 'next-auth'
 import React from 'react'
-import { DropdownMenu, DropdownMenuTrigger } from './ui/dropdown-menu'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
+import Link from 'next/link'
+import { signOut } from 'next-auth/react'
+import { Button } from './ui/button';
+import { LogOut } from 'lucide-react';
 
 type Props = {
     user : Pick<User, "name" | "image" | "email">
@@ -10,8 +16,33 @@ const UserAccNav = ({user}: Props) => {
   return (
     <DropdownMenu>
         <DropdownMenuTrigger>
-            
+          {/* user avatar */}
         </DropdownMenuTrigger>
+        <DropdownMenuContent className="bg-white" align="end">
+          <div className="flex items-center justify-start gap-2 p-2">
+            <div className="flex flex-col space-y-1 leading-none">
+              {user.name && <p className="font-medium">{user.name}</p>}
+              {user.email && (
+                <p className="w-[200px] truncate text-sm text-zinc-700">
+                  {user.email}
+                </p>
+              )}
+            </div>
+          </div>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem asChild>
+            <Link href='/'>Meow</Link>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={(e) => {
+            e.preventDefault()
+            signOut().catch(console.error)
+          }} className="text-red-800 cursor-pointer"
+          >
+            Sign Out
+            <LogOut className="w-4 h-4 ml-2 " />
+          </DropdownMenuItem>
+        </DropdownMenuContent>
     </DropdownMenu>
   )
 }
---
+++ src/components/UserAvatar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const UserAvatar = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UserAvatar
